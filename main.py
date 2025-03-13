49f46d40896e9f9d70d1ea71c42c81d403dc3de4
osmargsilva <osmar.gsilva87@gmail.com>
2025-03-13 17:31:37 +0000

    ok

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from Conn import Conn
+class ContaCorrente:
 
+    def __init__(self, nome, cpf):
+        self.nome = nome
+        self.cpf = cpf
+        self.salto = 0
 
-try:
-    conn = Conn()
-    conn.db_connect()
-    print('ok')
-except Exception as error:
-    print(error)
+    def extrado(self):
+        return f' seu saldo é R${self.salto:,.2f}'
+
+    def depositar(self, deposito):
+        self.salto += deposito
+    
+    def sacar(self, saque):
+        if self.salto >= saque:
+            self.salto -=  saque
+            return f"Saque realizado com successo no valor R${saque:,.2f}"
+        else:
+            return self.extrado()
+    
+
+conta_a = ContaCorrente("osmar", "123456789")
+
+conta_a.depositar(750.50)
+
+print(conta_a.extrado())
+print(conta_a.sacar(790))
